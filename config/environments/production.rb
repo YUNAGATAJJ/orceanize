e662d8972dead4439dba26a869838b79cc95a360
YUNAGATAJJ <xperia.09255@gmail.com>
2023-12-16 23:46:22 +0900

    added config.assets.css_compressor to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #12/16追記
+  config.assets.css_compressor = nil
 end
