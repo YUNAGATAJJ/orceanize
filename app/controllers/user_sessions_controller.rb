96b1cb6b7334f7c16fcf9baa1469fc9abe88711a
YUNAGATAJJ <xperia.09255@gmail.com>
2024-01-04 14:32:06 +0900

    addon pg on heroku and created flashmessage

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,14 +6,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path
+      redirect_to root_path, success: 'ログインしました'
     else
-      render :new
+      flash.now[:danger] = "ログインできませんでした"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, success: 'ログアウトしました'
   end
 end
