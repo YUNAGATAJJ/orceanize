8775075218e6c27b3ae75d5c015f5edd7da1d139
YUNAGATAJJ <xperia.09255@gmail.com>
2024-01-18 15:09:35 +0900

    resize default.jpg

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -33,6 +33,10 @@ class PostImageUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
+  version :index_size do
+    process resize_and_pad: [1600, 900, '#f5ebdc', 'Center']
+  end
+
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_allowlist
