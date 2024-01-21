77a08a0e47d3a20b5e786ed67e7a0f36dec34c34
YUNAGATAJJ <xperia.09255@gmail.com>
2024-01-21 10:38:18 +0900

    setup credentials

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -2,9 +2,11 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # 以下コメントアウト
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
