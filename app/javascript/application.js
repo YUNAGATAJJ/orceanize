a2c6997704ccb70baab388670e0af35ce10b6c85
YUNAGATAJJ <xperia.09255@gmail.com>
2024-01-24 23:37:24 +0900

    fixed jsimport

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./email_confirmation"
-import "./canva"
+// import "./email_confirmation"
+// import "./canva"
+import "./javascript/email_confirmation"
+import "./javascript/canva"
