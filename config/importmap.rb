fbc82d100da24e2c3ee1f76d82db66b6452c8c16
YUNAGATAJJ <xperia.09255@gmail.com>
2024-01-25 11:25:29 +0900

    fixed jsimport

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-#下記のようにしたらいい byらんてくん
-# pin "canva", to: "canva.js", preload: true
-# pin "email_confirmation", to: "email_confirmation.js", preload: true
+pin "canva", to: "canva.js", preload: true
+pin "email_confirmation", to: "email_confirmation.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
