7e3c63bf00fff561e3601f3a30d8451da6d6d384
YUNAGATAJJ <xperia.09255@gmail.com>
2024-02-09 22:16:42 +0900

    rubocup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
   skip_before_action :require_login, only: [:new, :create, :update, :destroy]
 
   def show; end
@@ -12,33 +12,34 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-      if @user.save
-        redirect_to root_path, success: '新規ユーザを作成しました'
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @user.save
+      redirect_to root_path, success: '新規ユーザを作成しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
-      if @user.update(user_params)
-        redirect_to user_url(@user), success: "ユーザ情報が更新されました"
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    if @user.update(user_params)
+      redirect_to user_url(@user), success: "ユーザ情報が更新されました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @user.destroy!
-      redirect_to users_url, success: "ユーザを削除しました."
-      format.json { head :no_content }
+    redirect_to users_url, success: "ユーザを削除しました."
+    format.json { head :no_content }
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    def user_params
-      params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
 end
