ae6720b1dd49e4f86e7b32d9daf08d56beb8638a
YUNAGATAJJ <xperia.09255@gmail.com>
2024-02-11 00:01:12 +0900

    gallary path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
+  skip_before_action :require_login, only: %i[index show]
 
   def index
     @q = Post.ransack(params[:q])
