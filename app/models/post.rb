2ecdc51179a845e8ba9d92d89d3e11973b44cf3c
YUNAGATAJJ <xperia.09255@gmail.com>
2024-03-02 16:55:56 +0900

    created profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   mount_uploader :image, PostImageUploader
+  belongs_to :user
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   validates :title, presence: true, length: { maximum: 25 }
